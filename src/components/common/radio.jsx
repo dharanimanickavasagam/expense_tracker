2238f17bb24985d39ef8db5730d17cd4d36726c6
dharani <dhaaru1992@gmail.com>
2019-09-15 14:11:28 -0400

    mockbackend

---
+++ src/components/common/radio.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Radio = ({
+	type,
+	labelFor,
+	labelName,
+	inputId,
+	onChange,
+	options,
+	...rest
+}) => {
+	return (
+		<div className="col-auto">
+			<label htmlFor={labelFor}>{labelName}</label>
+			{options.map(option => (
+				<div>
+					<input
+						type={type}
+						id={inputId}
+						onChange={onChange}
+						autoComplete="off"
+						name={inputId}
+						value={option}
+						{...rest}
+					/>
+					<span> {option} </span>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Radio;
