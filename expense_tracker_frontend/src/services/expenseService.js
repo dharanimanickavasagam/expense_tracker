b63fc61aa6db4a2b9254fdfa0013b3170425d88f
dharani <dhaaru1992@gmail.com>
2019-09-30 09:30:21 -0400

    changing backend

--- expense_tracker_frontend/src/services/expenseService.js
+++ expense_tracker_frontend/src/services/expenseService.js
@@ -2,21 +2,21 @@ import http from "../httpAxiosModule/httpAxiosModule";
 import config from "../config.json";
 
 export const getExpenseService = () => {
-    return http.get(config.apiEndPoint + "/expenses");
+    return http.get(config.apiEndPoint + "/expense");
 }
 
 export const addExpenseService = (expense) => {
-    return http.post(config.apiEndPoint + "/expenses", expense);
+    return http.post(config.apiEndPoint + "/expense", expense);
 }
 
 export const deleteExpenseService = (expenseID) => {
     console.log("Service", expenseID)
-    return http.delete(config.apiEndPoint + "/expenses/" +
+    return http.delete(config.apiEndPoint + "/expense/" +
         `${expenseID}`);
 }
 
 export const updateExpenseService = (expense) => {
     const id = expense.id;
-    return http.put(config.apiEndPoint + "/expenses/" +
+    return http.put(config.apiEndPoint + "/expense/" +
         `${id}`, expense);
 }
--- expense_tracker_frontend/src/services/expenseTypeService.js
+++ expense_tracker_frontend/src/services/expenseTypeService.js
@@ -2,20 +2,20 @@ import http from "../httpAxiosModule/httpAxiosModule";
 import api from "../config.json"
 
 export const getExpenseTypeService = () => {
-    return http.get(api.apiEndPoint + "/expenseTypes");
+    return http.get(api.apiEndPoint + "/expenseType");
 }
 
 export const addExpenseTypeService = (expenseType) => {
-    return http.post(api.apiEndPoint + "/expenseTypes", expenseType);
+    return http.post(api.apiEndPoint + "/expenseType", expenseType);
 }
 
 export const deleteExpenseTypeService = (expenseTypeID) => {
-    return http.delete(api.apiEndPoint + "/expenseTypes/" +
+    return http.delete(api.apiEndPoint + "/expenseType/" +
         `${expenseTypeID}`);
 }
 
 export const updateExpenseTypeService = (expenseType) => {
     const id = expenseType.id;
-    return http.put(api.apiEndPoint + "/expenseTypes/" +
+    return http.put(api.apiEndPoint + "/expenseType/" +
         `${id}`, expenseType);
 }
