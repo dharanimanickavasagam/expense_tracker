43228fed3624d6ec60566325fe023ce678c1589a
dharani <dhaaru1992@gmail.com>
2019-11-20 11:01:19 -0500

    spacing

--- expense_tracker_frontend/src/actions/expenseType.js
+++ expense_tracker_frontend/src/actions/expenseType.js
@@ -12,51 +12,46 @@ import {
   updateExpenseTypeService
 } from "../services/expenseTypeService";
 
-
 export const getExpenseType = () => {
-  return function (dispatch) {
-    return getExpenseTypeService()
-      .then(data => {
-        dispatch({
-          type: GET_EXPENSE_TYPE,
-          payload: data
-        });
+  return function(dispatch) {
+    return getExpenseTypeService().then(data => {
+      dispatch({
+        type: GET_EXPENSE_TYPE,
+        payload: data
       });
+    });
   };
 };
 
 export const addExpenseType = payload => {
-  return function (dispatch) {
-    return addExpenseTypeService(payload)
-      .then( data => {
-        dispatch({
-          type: ADD_EXPENSE_TYPE,
-          payload: data
-        })
-      })
+  return function(dispatch) {
+    return addExpenseTypeService(payload).then(data => {
+      dispatch({
+        type: ADD_EXPENSE_TYPE,
+        payload: data
+      });
+    });
   };
 };
 
 export const updateExpenseType = payload => {
-  return function (dispatch) {
-    return updateExpenseTypeService(payload)
-      .then( data => {
-          dispatch({
-          type: UPDATE_EXPENSE_TYPE,
-          payload: data
-        })
+  return function(dispatch) {
+    return updateExpenseTypeService(payload).then(data => {
+      dispatch({
+        type: UPDATE_EXPENSE_TYPE,
+        payload: data
       });
-  }
+    });
+  };
 };
 
 export const deleteExpenseType = payload => {
-  return function (dispatch) {
-    return deleteExpenseTypeService(payload)
-      .then( data => 
-        {dispatch({
-          type: DELETE_EXPENSE_TYPE,
-          payload: data
-        })}
-      );
+  return function(dispatch) {
+    return deleteExpenseTypeService(payload).then(data => {
+      dispatch({
+        type: DELETE_EXPENSE_TYPE,
+        payload: data
+      });
+    });
   };
 };
