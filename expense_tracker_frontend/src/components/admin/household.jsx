bf1e50102139e6efe205e0029d47052b85aa58d6
dharani <dhaaru1992@gmail.com>
2019-11-20 15:49:03 -0500

    adding currency selector

--- expense_tracker_frontend/src/components/admin/household.jsx
+++ expense_tracker_frontend/src/components/admin/household.jsx
@@ -1,26 +1,44 @@
-import React, { Component } from 'react';
-import FormControl from '@material-ui/core/FormControl';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import TextField from '@material-ui/core/TextField';
+import React, { Component } from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import _ from "lodash";
 
 class Household extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <FormControl>
-                <TextField
-                        id="standard-name"
-                        label="Email"
-                        style={{ marginRight : "2em"}}
-                        //value={name}
-                        //onChange={}
-                        margin="normal"
-                        />
-                <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-            </FormControl>
-        );
-    }
+  state = {
+    currencyFormat: [{ USD: "$" }, { AUD: "$" }, { INR: "₹" }, { Euro: "€" }],
+    selectedCurrency: ""
+  };
+
+  handleCurrencyFormat = event => {
+    const selectedCurrency = event.target.value;
+    this.setState({ selectedCurrency });
+  };
+
+  render() {
+    return (
+      <FormControl fullWidth>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Currency Format
+        </InputLabel>
+
+        <Select
+          labelId="demo-simple-select-placeholder-label-label"
+          id="demo-simple-select-placeholder-label"
+          onChange={this.handleCurrencyFormat}
+          value={this.state.selectedCurrency}
+          displayEmpty
+        >
+          {this.state.currencyFormat.map(currencyFormat => (
+            <MenuItem value={_.values(currencyFormat)[0]}>
+              {`${_.keys(currencyFormat)} : ${_.values(currencyFormat)}`}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    );
+  }
 }
- 
+
 export default Household;
